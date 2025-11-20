bd09fb16a3289d68d5b82dc7eeabcafd148a54fe
Davlatyar"git config --global user.name Davlatyargit config --global user.name Davlatyar <davlatyar2008@gmail.com>
2025-11-20 17:32:25 +0500

    done

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const TelegramBot = require("node-telegram-bot-api")
+import TelegramBot from "node-telegram-bot-api"
 
 const TOKEN = "8053059566:AAHzxLSE-aFLpASry8W6WUxnQbUIMoJ_WnE"
 
 const bot = new TelegramBot(TOKEN, { polling:true })
 
+import { config } from "dotenv";
+
 bot.on("message", (msg) => {
+  
     const ChatId = msg.chat.id;
     const text = msg.text;
+    console.log(msg);
+    
+    if (text == "/start") {
+        bot.sendMessage(ChatId, 
+            `Salom ${msg.from.first_name} 100x o‘quv markazining rasmiy botiga xush kelibsiz!
 
-    bot.sendMessage(ChatId,"salom.")
+        Bu bot orqali siz:
+        • Kurslarimiz haqida batafsil ma’lumot olasiz  
+        • Kurslarga onlayn ro‘yxatdan o‘tishingiz mumkin  
+        • Jadval va to‘lovlar haqida ma’lumot olasiz  
+
+Quyidagi menyudan kerakli bo‘limni tanlang 👇
+!
+            `,
+
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                [{ text: "Ingliz tili", callback_data: "course_english" }],
+                [{ text: "Russ tili", callback_data: "course_Russian" }],
+                [{ text: "German tili", callback_data: "course_German" }],
+                    ]
+                }
+            }
+        )
+    }
+})
+bot.on("callback_query", function (query) {
+    const data = query.data
+    const chatId = query.message.chat.id
+
+    if (data == "course_english") {
+        bot.sendMessage(chatId, 'Uztozning ismi: Maxdiy \nIELTS: C1 \nYears expirience: 3 years')
+    }else if (data == "course_Russian") {
+        bot.sendMessage(chatId, ``)
+    }
+
 })

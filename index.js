22417cd086cf2027b4d9a9edf1c30cb13a6c4749
Davlatyar"git config --global user.name Davlatyargit config --global user.name Davlatyar <davlatyar2008@gmail.com>
2025-11-25 17:23:18 +0500

    done

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import TelegramBot from "node-telegram-bot-api"
 import { config } from "dotenv";
+import mongoose from "mongoose";
 
 config();
 const TOKEN = process.env.BOT_TOKEN;
 const bot = new TelegramBot(TOKEN, { polling:true })
 
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("DB is connected successfully...");
+})
+.catch(()=> {
+    console.log("Error: Db is not connected...");
+})
 
 bot.on("message", (msg) => {
   
@@ -28,10 +37,15 @@ Quyidagi menyudan kerakli bo‘limni tanlang 👇
             {
                 reply_markup: {
                     inline_keyboard: [
-                [{ text: "Ingliz tili", callback_data: "course_english" }],
-                [{ text: "Russ tili", callback_data: "course_Russian" }],
-                [{ text: "German tili", callback_data: "course_German" }],
-                    ]
+                        [
+                            {text: "📚 Kurslar", callback_data: "course"},
+                            {text: "✍️ Ro‘yxatdan o‘tish", callback_data: "register"},
+                        ],
+                        [
+                            {text: "ℹ️ Markaz haqida", callback_data: "location"},
+                            {text: "❓ Yordam", callback_data: "help"},
+                        ]
+                    ],
                 }
             }
         )
@@ -41,10 +55,34 @@ bot.on("callback_query", function (query) {
     const data = query.data
     const chatId = query.message.chat.id
 
-    if (data == "course_english") {
-        bot.sendMessage(chatId, 'Uztozning ismi: Maxdiy \nIELTS: C1 \nYears expirience: 3 years')
-    }else if (data == "course_Russian") {
-        bot.sendMessage(chatId, ``)
+    if (data === "course") {
+        bot.sendMessage(chatId, `🎓 Bizning o‘quv markazimizda quyidagi kurslar mavjud:
+
+1️⃣ Ingliz tili  
+2️⃣ Rus tili  
+3️⃣ Matematika  
+4️⃣ Dasturlash (Python, Web)  
+5️⃣ Grafik dizayn  
+
+👇 Quyidagi kurslardan birini tanlang va batafsil ma’lumot oling:`,
+
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{text: "English kursi", callback_data: "course_English"}],
+                        [{text: "Russ tili kursi", callback_data: "course_English"}]   
+                    ]
+                }
+            }
+        )
     }
+
+    // if (data == "course_english") {
+    //     bot.sendMessage(chatId, 'Uztozning ismi: Maxdiy \nIELTS: C1 \nYears expirience: 3 years')
+    // }else if (data == "course_Russian") {
+    //     bot.sendMessage(chatId, `Uztozning ismi: Maxdiy\nYears expirience: 4 years`)
+    // }else if (data == "course_German") {
+    //     bot.sendMessage(chatId, `Uztozning ismi: Maxdiy\nYears expirience: 6 years`)
+    // }
 
 })
